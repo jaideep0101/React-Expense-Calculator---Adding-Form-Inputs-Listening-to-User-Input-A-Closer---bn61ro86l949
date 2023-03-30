eb6e42021fb366f3d7ef084dbc1fa7fd98b7e0ec
Jaideep singh thakur <88130081+jaideep0101@users.noreply.github.com>
2023-03-30 12:56:11 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,40 @@
-import React from 'react'
+import React from 'react';
 import '../styles/App.css';
 
 const App = () => {
+  const [expenses, setExpenses] = React.useState([]);
+
+  const handleAddExpense = () => {
+    const expenseInput = document.getElementById('expense-input');
+    const expenseText = expenseInput.value.trim();
+    if (expenseText !== '') {
+      const [itemName, itemPrice] = expenseText.split(' - ');
+      if (itemName && itemPrice) {
+        const price = parseFloat(itemPrice);
+        const newExpense = {
+          name: itemName,
+          price: price,
+        };
+        setExpenses(expenses => [...expenses, newExpense]);
+        expenseInput.value = '';
+      }
+    }
+  };
 
   return (
     <div id="main">
       <input id="expense-input" />
-      <button id="expense-button">Add Expense</button>
+      <button id="expense-button" onClick={handleAddExpense}>Add Expense</button>
       <div id="expense-list">
+        {expenses.map((expense, index) => (
+          <div key={index}>{expense.name} - {expense.price}</div>
+        ))}
       </div>
       <div id="total-expense">
-        Total Expense: 0
+        Total Expense: {expenses.reduce((total, expense) => total + expense.price, 0)}
       </div>
     </div>
-  )
-}
+  );
+};
 
-
-export default App;
+export default App;
